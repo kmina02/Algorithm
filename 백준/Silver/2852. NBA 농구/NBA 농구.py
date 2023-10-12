8f806d85026262fee3b23cb33018246ddbd6843b
kmina02 <79454352+kmina02@users.noreply.github.com>
2023-10-12 14:48:27 +0900

    Update NBA 농구.py

--- 백준/Silver/2852. NBA 농구/NBA 농구.py
+++ 백준/Silver/2852. NBA 농구/NBA 농구.py
@@ -1,33 +1,40 @@
-goal = int(input())
-team_one_score = 0
-team_two_score = 0
-team_one_time = 0
-team_two_time = 0
-time_score_dic = dict()
- 
-for _ in range(goal):
-  team, win_time = input().split()
-  m, s = map(int, win_time.split(":"))
-    
-  time_score_dic[m * 60 + s] = int(team)
- 
-for i in range(48 * 60):
-    if i in time_score_dic:
-      if time_score_dic[i] == 1:  
-        team_one_score += 1
-      else:
-        team_two_score += 1
-    
-    if team_one_score > team_two_score:
-      team_one_time += 1
-    elif team_one_score < team_two_score:
-      team_two_time += 1
-    
-team_one_m = team_one_time // 60
-team_one_s = team_one_time % 60
- 
-team_two_m = team_two_time // 60
-team_two_s = team_two_time % 60
- 
-print("{:02d}:{:02d}".format(team_one_m, team_one_s))
-print("{:02d}:{:02d}".format(team_two_m, team_two_s))
+import datetime
+# 입력, 변수 설정
+N = int(input())
+score = []
+times = [0]
+t1, t2 = 0, 0
+s1, s2 = 0, 0
+
+# 시간(문자열)을 분으로 바꿔주는 함수
+def minute(x):
+  m, s = map(int, x.split(':'))
+  x = m*60 + s
+  return(x)
+
+# 분을 시간(문자열)로 바꿔주는 함수
+def time(x):
+  m = x // 60
+  s = x % 60
+  print("{:02d}:{:02d}".format(m, s))
+
+# 반복문으로 정보 입력
+for i in range(N):
+  n, t = input().split()
+  if n=='1': s1+=1
+  else: s2+=1
+  times.append(minute(t))
+  score.append([s1, s2])
+times.append(2880)
+
+# 반복문으로 시간 계산
+for j in range(N):
+  if score[j][0]>score[j][1]:
+    t1+=(times[j+2] - times[j+1])
+  elif score[j][0]<score[j][1]:
+    t2+=(times[j+2] - times[j+1])
+  else: continue
+
+# 출력
+time(t1)
+time(t2)
