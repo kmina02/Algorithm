ef7b902cf5f8897760b0e4d0a05d60241212daf4
kmina02 <79454352+kmina02@users.noreply.github.com>
2024-02-23 16:15:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,3 +1,7 @@
 def solution(array, commands):
     answer = []
+    for c in commands:
+        i, j, k = c[0], c[1], c[2]
+        result = sorted(array[i-1:j])[k-1]
+        answer.append(result)
     return answer
