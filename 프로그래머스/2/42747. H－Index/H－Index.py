241e65c1fc658acc6257790075ae8dbe369136d9
kmina02 <79454352+kmina02@users.noreply.github.com>
2024-02-23 16:47:30 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,7 @@
 def solution(citations):
     citations.sort(reverse = True)
     for i in range(len(citations)):  
-        if i+1 > citations[i]:
+        if i+1 >= citations[i]:
             return i
     return len(citations)
         
-        
-        # 6 5 3 1 0
-        # 8 8 4 3 2 1
-        # i = 0 1 2 3 4
