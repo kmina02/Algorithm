96cc5a6e3e853c8f94d870c978f7003b24a2fea4
kmina02 <79454352+kmina02@users.noreply.github.com>
2024-07-02 22:06:34 +0900

    [level 2] Title: 소수 찾기, Time: 0.66 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,9 +1,25 @@
 from itertools import permutations
+
 def solution(numbers):
-    a = set()
-    for i in range(len(numbers)):
-        a |= set(map(int, map("".join, permutations(list(numbers), i+1))))
-    a -= set(range(0, 2))
-    for i in range(2, int(max(a)**0.5) + 1):
-        a -= set(range(i*2, max(a) + 1, i))
-    return len(a)
+    # numbers를 리스트로 만든다.
+    num_list = [i for i in numbers] 
+    
+    # 만들 수 있는 모든 경우의 수를 만든다.
+    can_list = []
+    for n in range(1, len(num_list) + 1):
+        for c in permutations(num_list, n):
+            can_list.append(int(''.join(c))) 
+    # 중복을 제거한다.
+    can_list = sorted(list(set(can_list)))
+    print(can_list)
+    if 0 in can_list:
+        can_list.remove(0)
+    if 1 in can_list:
+        can_list.remove(1)
+    # 소수 여부를 확인한다.      
+    for c in can_list[:]:
+        for i in range(2, c):
+            if c % i == 0:
+                can_list.remove(c)
+                break
+    return len(can_list)
