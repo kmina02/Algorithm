93bca3622c1e0e6b09d8e88b0fbfb6335ad60890
kmina02 <79454352+kmina02@users.noreply.github.com>
2024-07-02 15:55:12 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,11 +1,10 @@
 -- 코드를 작성해주세요
 SELECT
     ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM 
+FROM
     DEVELOPERS
 WHERE
-    SKILL_CODE & (select code from skillcodes where name = 'Python') 
-    OR
-    SKILL_CODE & (select code from skillcodes where name = 'C#') 
+    (SKILL_CODE & (select code from skillcodes where name = 'Python')) OR
+    (SKILL_CODE & (select code from skillcodes where name = 'C#'))
 ORDER BY
     ID
