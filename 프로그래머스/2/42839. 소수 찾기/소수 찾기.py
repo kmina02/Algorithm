01cdc2d75b6c9527ba7785b9b503e5daa0066875
kmina02 <79454352+kmina02@users.noreply.github.com>
2024-07-04 12:39:24 +0900

    [level 2] Title: 소수 찾기, Time: 0.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,27 +1,23 @@
 from itertools import permutations
+
 def is_prime(x):
+    if x < 2:
+        return False
     for i in range(2, int(x**0.5) + 1):
         if x % i == 0:
             return False
     return True
+
 def solution(numbers):
-    answer = 0
     # numbers를 리스트로 만든다.
     num_list = list(numbers)
     
     # 만들 수 있는 모든 경우의 수를 만든다.
-    can_list = []
+    can_list = set()
     for i in range(1, len(num_list) + 1):
-        can_list += list(permutations(num_list, i))
-    can_list = [int(''.join(c)) for c in can_list]
-    # 중복을 제거한다.
-    can_list = sorted(list(set(can_list)))
+        can_list.update(int(''.join(c)) for c in permutations(num_list, i))
 
-    # 소수 여부를 확인한다.      
-    for c in can_list:
-        if c <= 1:
-            continue
-        elif is_prime(c):
-            answer += 1
+    # 소수 여부를 확인한다.
+    answer = sum(1 for c in can_list if is_prime(c))
             
     return answer
