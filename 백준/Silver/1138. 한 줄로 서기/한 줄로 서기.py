1b36eb574842a5239f64199ed13734cdac8058da
kmina02 <79454352+kmina02@users.noreply.github.com>
2024-07-04 14:37:36 +0900

    [Silver II] Title: 한 줄로 서기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -1,18 +1,3 @@
-n = int(input())
-height = [0] + list(map(int, input().split()))
-answer = []
-
-for k in range(n, 0, -1): 
-    target = k
-    info = height[k]
-    count = 0
-    if info == 0:
-        answer.insert(0, target)
-    else:
-        for i in range(len(answer)):
-            if answer[i] > target: 
-                count += 1
-            if count == info:
-                answer.insert(i+1, target)
-                break
-print(' '.join([str(a) for a in answer]))
+r=[];n=int(input())
+for q in input().split()[::-1]:r.insert(int(q),n);n-=1
+print(*r)
